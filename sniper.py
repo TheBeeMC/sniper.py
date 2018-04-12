c69787492a09de63efc2f23945805b2a8129ec21
iliyan <hi@iliyan.me>
2018-04-12 13:36:32 -0400

    Update for archival

--- sniper.py
+++ sniper.py
@@ -9,7 +9,7 @@ import logging
 
 command_prefix='+'
 bot = commands.Bot(command_prefix)
-description = 'sniper.py, coded by trinity-y and ili-codes'
+description = 'sniper.py, coded by unpredictable'
  
 @bot.event
 async def on_message(message):
@@ -17,14 +17,14 @@ async def on_message(message):
             embed = discord.Embed(title="Let's walk you through the process of queueing a snipe.", description="DM the bot with 'beginqueue' to queue your snipe.", colour=0x1a94f0)
             embed.set_author(name="Thanks for choosing sniper.py!", icon_url="")
             embed.add_field(name="Enjoy your new name!", value="-trinity and iliyan")
-            embed.set_footer(text="sniper.py™ © coded by trinity-y and ili-codes")
+            embed.set_footer(text="sniper.py™ © coded by unpredictable")
             await bot.send_message(message.channel, embed=embed)
 
     if message.content.startswith('+help'):
         embed=discord.Embed(title="+snipeadd: queues a snipe", description="+invite: Prints an invite link for the bot.", color=0x1a94f0)
         embed.set_author(name='Commands', icon_url="")
         embed.add_field(name="+help: Brings up list of commands", value="+info: Prints info about the bot.", inline=True)
-        embed.set_footer(text='coded by trinity-y and ili-codes')
+        embed.set_footer(text='coded by unpredictable')
         await bot.send_message(message.channel, embed=embed)
 
     if message.content.startswith('+invite'):
@@ -32,13 +32,13 @@ async def on_message(message):
         print('Bot has been added to new server')
 
     if message.content.startswith('+info'):
-        await bot.send_message(message.channel, "Coded by trinity and iliyan. Bot hosted on Heroku, coded with python3 and Discord.PY.")
+        await bot.send_message(message.channel, "Coded by unpredictable. Bot hosted on Heroku, coded with python3 and Discord.PY.")
 
 async def status_task():
     while True:
         await bot.change_presence(game=discord.Game(name="with og names"))
         await asyncio.sleep(10)
-        await bot.change_presence(game=discord.Game(name="+help | coded by trin and ili"))
+        await bot.change_presence(game=discord.Game(name="+help | coded by ili"))
         await asyncio.sleep(10)
         await bot.change_presence(game=discord.Game(name="with my sniper"))
         await asyncio.sleep(10)
@@ -47,7 +47,7 @@ async def status_task():
 
 @bot.event
 async def on_ready():
-    print('sniper.py coded by ili-codes and trinity-y')
+    print('sniper.py coded by unpredictable')
     print('------')
     print('INFO')
     print('------')
